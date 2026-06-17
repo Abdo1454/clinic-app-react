a84ecdb1ede0f2c3d7909bc27542e6f8b8ee7c48
Abdulrahim <abdulrhimelsayed@gmail.com>
2026-06-17 23:57:54 +0300

    update section about and add info

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "./About.css";
 import Doctor from "../../assets/img/image-DR.png";
+import Care1 from "../../assets/img/corridoor.png";
+import Care2 from "../../assets/img/analysis.png";
 function About() {
   return (
     <div>
@@ -26,6 +28,50 @@ function About() {
           <i className="bi bi-patch-check"></i>  Board Certified in Internal Medicine
           </span>
         </div>
+      </section>
+      <section className="info-about">
+        <div className="info-our">
+          <h2>Why Patients Choose MediCore</h2>
+        <div className="list-info">
+          <i className="fa-solid fa-clock"></i>
+          <div className="info-small">
+
+           <h4>Zero Wait Policy</h4>
+           <p>We value your time as much as your health. Punctuality is our promise.</p>
+          </div>
+          </div>
+        <div className="list-info">
+          <i className="fa-solid fa-prescription-bottle-medical"></i>
+            <div className="info-small">
+
+           <h4>Zero Wait Policy</h4>
+           <p>Equipped with the latest diagnostic and therapeutic technologies</p>
+          </div>
+          </div>
+        <div className="list-info">
+<i className="fa-solid fa-user-doctor"></i>
+          <div className="info-small">
+
+           <h4>Zero Wait Policy</h4>
+           <p>Highly specialized medical professionals dedicated to your recovery.</p>
+          </div>
+          </div>
+        </div>
+        <div className="show-info-about">
+          <div className="care-1">
+            <img src={Care1} alt="Care" />
+            <p> <span>98%</span> <br />
+PATIENT SATISFACTION</p>
+          </div>
+
+          <div className="care-2">
+            <p> <span>24/7</span> <br />
+EMERGENCY CARE
+</p>
+            <img src={Care2} alt="Care" />
+          </div>
+        </div>
+
       </section>
     </div>
   );
