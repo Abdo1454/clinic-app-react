2a5c8770c4a610598b0e8fb8b20b6f8be14d8998
Abdulrahim <abdulrhimelsayed@gmail.com>
2026-06-23 12:19:33 +0300

    fix(navbar): fix mobile menu icon visibility and styles on desktop

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,22 +1,36 @@
-import React from 'react';
+import { useState }   from 'react';
 import "./Navbar.css";
 function Navbar() {
+    const [menu,setMenu]=useState(false);
   return (
     <div>
         <nav className="navbar">
             <h2 className="logo">MediCore</h2>
-         <div className="menu-icon">
+         <div id='menu-mobile'
+         
+        // className="menu-icon"
+        className={`menu-icon ${menu ? "open" : ""}`}
+        onClick={() => setMenu(!menu)}
+         >
   <span className="dash"></span>
   {/* <br></br> */}
   <span className="dash"></span>
   {/* <br></br> */}
   <span className="dash"></span>
 </div>
-            <ul className="nav-links">
-                <li><a href="#">Services</a></li>
-                <li><a href="#">About</a></li>
-                <li><a href="#">Testimonials</a></li>
-                <li><a href="#">FAQ</a></li>
+            <ul className={`nav-links ${menu ? "active" : ""}`}>
+                <li><a href="#" 
+                onClick={()=>setMenu(false)}
+                >Services</a></li>
+                <li><a href="#"
+                onClick={()=>setMenu(false)}
+                >About</a></li>
+                <li><a href="#"
+                onClick={()=>setMenu(false)}
+                >Testimonials</a></li>
+                <li><a href="#"
+                onClick={()=>setMenu(false)}
+                >FAQ</a></li>
                 <li className="btn-book"><a href="#">Book Now</a></li>
             </ul>
         </nav>
